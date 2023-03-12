b19ee056f40d9e29fec8c3eb446beacad5b07330
alimukhammad <47307714+alimukhammad@users.noreply.github.com>
2023-03-11 23:57:10 -0500

    Add files via upload

---
+++ SumAnB.java
@@ -0,0 +1,17 @@
+
+public class SumAnB{
+
+    public static void main(String[]args){
+
+
+        Scanner sc = new Scanner(Integer.in);
+        
+        System.out.println("Enter integer value A: ");
+        Integer A = myObj.nextLine();
+
+        System.out.println("Enter integer value B: ");
+        Integer B = myObj.nextLine();
+        
+        System.out.println(A, " + ", B " = ", A+B);
+    }
+}
